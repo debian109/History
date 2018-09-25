63e95741f567a113f5683bbd1c6010578b9ba13c
Vo Xuan Thinh <thinh.vo@sutrixsolutions.com>
2018-09-25 18:45:20 +0700

    init

---
+++ Assets/Script/BoardControl.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BoardControl : MonoBehaviour
+{
+    public static BoardControl instance = null;
+
+    public int width;
+
+    public int height;
+
+    public Transform tilePrefabs;
+
+    public GameObject[,] map;
+
+    public List<GameObject> list = new List<GameObject>();
+
+    // Use this for initialization
+    void Start()
+    {
+        map = new GameObject[width, height];
+
+        for (int j = 0; j < height; j++)
+        {
+            for (int i = 0; i < width; i++)
+            {
+                var tile = Instantiate(tilePrefabs, transform.position, transform.rotation);
+                tile.transform.SetParent(transform);
+                tile.localScale = Vector3.one;
+                var component = tile.GetComponent<TileControl>();
+                component.x = j;
+                component.y = i;
+                map[i, j] = tile.gameObject;
+                list.Add(tile.gameObject);
+            }
+
+        }
+
+        data();
+
+        Invoke("Disable", 1.0f);
+
+    }
+
+    public void Disable()
+    {
+        GetComponent<GridLayoutGroup>().enabled = false;
+    }
+
+    public void data()
+    {
+        List<GameObject> temp = new List<GameObject>();
+        int length = list.Count / 2;
+        for (int i = 0; i < length; i++)
+        {
+            int r1=Random.Range(0, list.Count);
+            list[r1].GetComponent<TileControl>().type = i;
+            temp.Add(list[r1]);
+            list.RemoveAt(r1);
+
+            int r2 = Random.Range(0, list.Count);
+            list[r2].GetComponent<TileControl>().type = i;
+            temp.Add(list[r2]);
+            list.RemoveAt(r2);
+        }
+
+        list = temp;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Awake()
+    {
+        //Check if instance already exists
+        if (instance == null)
+
+            //if not, set instance to this
+            instance = this;
+
+        //If instance already exists and it's not this:
+        else if (instance != this)
+
+            //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+            Destroy(gameObject);
+          
+    }
+
+    public GameObject current;
+
+    public void Eat(GameObject go){
+        Debug.Log("v");
+        if (current == null)
+            current = go;
+        else{
+            CheckEat(current, go);
+            current = null;
+        }
+    }
+
+    public void CheckEat(GameObject t1,GameObject t2){
+        var c1 = t1.GetComponent<TileControl>();
+        var c2 = t2.GetComponent<TileControl>();
+        if(c1.type==c2.type){
+            c1.gameObject.SetActive(false);
+            c2.gameObject.SetActive(false);
+        }else{
+            c1.Revert();
+            c2.Revert();
+        }
+    }
+}
---
+++ Assets/Script/TileControl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TileControl : MonoBehaviour {
+
+    public List<Sprite> list = new List<Sprite>();
+
+    public int type;
+
+    public int x;
+
+    public int y;
+
+    public Image image;
+
+    public bool active;
+
+	// Use this for initialization
+	void Start () {
+        if(type>0 && type<list.Count){
+            GetComponent<Image>().sprite = list[type];
+        }
+        GetComponent<Button>().onClick.AddListener(OnClick);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnClick(){
+        Revert();
+        Invoke("Eat", 0.5f);
+    }
+
+    void Eat(){
+        BoardControl.instance.Eat(gameObject);
+    }
+
+    public void Revert(){
+        active = !active;
+        image.gameObject.SetActive(active);
+    }
+}
